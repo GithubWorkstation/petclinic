5c11676de6dc33b6809826453a931afe14eb6e89
GithubWorkstation <79502250+GithubWorkstation@users.noreply.github.com>
2024-10-25 16:04:54 +0530

    Delete petclinic directory

--- petclinic/src/main/java/com/example/petclinic/repository/PetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.petclinic.repository;
-
-import com.example.petclinic.model.Pet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetRepository extends JpaRepository<Pet, Long> {
-}
--- petclinic/src/main/java/com/example/petclinic/repository/VisitRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.petclinic.repository;
-
-import com.example.petclinic.model.Visit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VisitRepository extends JpaRepository<Visit, Long> {
-}
